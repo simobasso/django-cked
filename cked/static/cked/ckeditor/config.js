3582a6a314b58ce7066d16ef3dec1a03fb62377e
simonebasso <thesolitary90@gmail.com>
2014-02-17 10:47:53 +0100

    config.js

--- cked/static/cked/ckeditor/config.js
+++ cked/static/cked/ckeditor/config.js
@@ -8,4 +8,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+    config.extraPlugins = 'vimeo,images,onchange';
+    config.removePlugins = 'iframe,image';
+    config.forceSimpleAmpersand = true;
 };
