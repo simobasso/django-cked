4d35cb4775a72685baf230da5c30cd5155b4b3c3
simonebasso <thesolitary90@gmail.com>
2014-03-07 13:38:33 +0100

    forzata la copia as plain text

--- cked/default_settings.py
+++ cked/default_settings.py
@@ -4,6 +4,7 @@ ELFINDER_DEFAULT_OPTIONS = {
 CKEDITOR_DEFAULT_OPTIONS = {
     'skin': 'moono',
     'toolbar': 'Full',
+    'forcePasteAsPlainText': true,
     'filebrowserWindowWidth': 940,
     'filebrowserWindowHeight': 450,
 }
@@ -11,6 +12,7 @@ CKEDITOR_DEFAULT_OPTIONS = {
 CKEDITOR_MINI_OPTIONS = {
     'skin': 'moono',
     'toolbar': 'Mini',
+    'forcePasteAsPlainText': true,
     'filebrowserWindowWidth': 940,
     'filebrowserWindowHeight': 450,
 }
